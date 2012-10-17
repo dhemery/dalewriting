4c362f45f383ef105ef44a821d334f102625ede2
Dale Emery <dale@dhemery.com>
2012-10-16 17:43:54 -0700

    Builds the whole site

---
+++ common/theme/_style/config.rb
@@ -0,0 +1,14 @@
+# Require any additional compass plugins here.
+project_type = :stand_alone
+
+# Publishing paths
+http_path = "../"
+http_images_path = "../images"
+css_dir = "../stylesheets"
+
+# Local development paths
+sass_dir = "."
+images_dir = "../images"
+
+line_comments = false
+output_style = :expanded
